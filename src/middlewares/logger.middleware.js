b2f94686c8105a2a1eb03f1b27490c1a1fb0e777
MohanSai111 <pvsmohan810@gmail.com>
2025-02-13 13:20:38 +0530

    commit

---
+++ src/middlewares/logger.middleware.js
@@ -0,0 +1,31 @@
+import fs from "fs";
+import winston from "winston";
+const fsPromise= fs.promises;
+
+// async function log(logData){
+//     try{
+//   logData= 
+//   `\n ${new Date().toString()}-${logData}`;
+//  await fsPromise.appendFile('log.txt',logData);
+//     }catch(err){
+//         console.error(err);
+//     }
+// }
+
+const logger= winston.createLogger({
+    level:"info",
+    format:winston.format.json(),
+    defaultMeta:{service:'user-service'},
+    transports:[
+        new winston.transports.File({filename:'logs.txt'})
+    ]
+})
+
+const logMiddleWare= async(req,res,next)=>{
+    if(!req.url.includes('signin')){
+   const logData= `${req.url}-${JSON.stringify(req.body)}`
+    logger.info(logData);
+    }
+   next();
+}
+export default logMiddleWare;
